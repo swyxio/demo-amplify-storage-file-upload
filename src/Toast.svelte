78a639289b4078a11c743300ae25c4af8a8a3b20
swyx <wanshawn@amazon.com>
2020-10-10 04:18:52 +0800

    fix toast

--- src/Toast.svelte
+++ src/Toast.svelte
@@ -9,20 +9,13 @@
   // } | undefined
 </script>
 
+<!-- for purgecss -->
+<!-- border-green-500
+border-red-500 -->
 {#if toast}
   <div
     transition:fly={{ duration: 300, x: 500, opacity: 0.5 }}
     class="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
-    <!--
-    Notification panel, show/hide based on alert state.
-
-    Entering: "transform ease-out duration-300 transition"
-      From: "translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-      To: "translate-y-0 opacity-100 sm:translate-x-0"
-    Leaving: "transition ease-in duration-100"
-      From: "opacity-100"
-      To: "opacity-0"
-  -->
     <div class="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto border-l-4"
   class:border-green-500={!toast.title.startsWith('Error')}
   class:border-red-500={toast.title.startsWith('Error')}
